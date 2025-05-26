707da8fbfce0686dbebb2ec04f3204b37753987a
andresdvx <andresdavidpachecocuadro@gmail.com>
2025-05-26 09:19:58 -0500

    add: integration with cart lambda

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ data "aws_lambda_function" "lambda_login" {
   function_name = "lambda_login"
 }
 
+
+data "aws_lambda_function" "lambda_create_cart" {
+  function_name = "createCart"
+}
 # Crear API Gateway HTTP
 resource "aws_apigatewayv2_api" "api_gateway" {
   name          = var.api_name
@@ -35,6 +39,15 @@ resource "aws_apigatewayv2_integration" "login_integration" {
   payload_format_version = "2.0"
 }
 
+# Integracion para lambda createCart
+resource "aws_apigatewayv2_integration" "create_cart_integration" {
+  api_id                 = aws_apigatewayv2_api.api_gateway.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = data.aws_lambda_function.lambda_create_cart.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
 # Ruta POST /user/register → lambda_register
 resource "aws_apigatewayv2_route" "register_route" {
   api_id    = aws_apigatewayv2_api.api_gateway.id
@@ -49,6 +62,13 @@ resource "aws_apigatewayv2_route" "login_route" {
   target    = "integrations/${aws_apigatewayv2_integration.login_integration.id}"
 }
 
+# Ruta POST /cart/create → lambda_create_cart
+resource "aws_apigatewayv2_route" "create_cart_route" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "POST /cart/create"
+  target    = "integrations/${aws_apigatewayv2_integration.create_cart_integration.id}"
+}
+
 # Implementación del deployment
 resource "aws_apigatewayv2_stage" "api_stage" {
   api_id      = aws_apigatewayv2_api.api_gateway.id
@@ -58,7 +78,7 @@ resource "aws_apigatewayv2_stage" "api_stage" {
 
 # Permisos para que API Gateway invoque lambda_register
 resource "aws_lambda_permission" "register_permission" {
-  statement_id  = "AllowAPIGatewayInvokeRegister"
+  statement_id  = "AllowAPIGatewayInvokeRegisterV2"
   action        = "lambda:InvokeFunction"
   function_name = data.aws_lambda_function.lambda_register.function_name
   principal     = "apigateway.amazonaws.com"
@@ -67,10 +87,18 @@ resource "aws_lambda_permission" "register_permission" {
 
 # Permisos para que API Gateway invoque lambda_login
 resource "aws_lambda_permission" "login_permission" {
-  statement_id  = "AllowAPIGatewayInvokeLogin"
+  statement_id  = "AllowAPIGatewayInvokeLoginV2"
   action        = "lambda:InvokeFunction"
   function_name = data.aws_lambda_function.lambda_login.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
 }
 
+# Permisos para que API Gateway invoque lambda_create_cart
+resource "aws_lambda_permission" "create_cart_permission" {
+  statement_id  = "AllowAPIGatewayInvokeCreateCart"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.lambda_create_cart.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+}
